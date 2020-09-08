93486c3c209848d5c8b09a40f69355d9da5ab382
lajtosdavid11 <lajtos.david11@gmail.com>
2020-09-08 11:16:50 +0200

    Számítógép választása

--- masodik/Program.cs
+++ masodik/Program.cs
@@ -10,7 +10,14 @@ namespace masodik
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello");
+            Random veletlen = new Random();
+            string[] lehetoseg = new string[] { "Kő", "Papír", "Olló" };
+
+            int gepValaszt = veletlen.Next(0,3);
+
+            Console.WriteLine("Gép választása: {0}",lehetoseg[gepValaszt]);
+            
+
 
             Console.ReadKey();
         }

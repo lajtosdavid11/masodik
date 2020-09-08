b94a2998d5a537c366e31a51a60ebd7a5463518c
lajtosdavid11 <lajtos.david11@gmail.com>
2020-09-08 11:22:13 +0200

    Felhasználó választ.

--- masodik/Program.cs
+++ masodik/Program.cs
@@ -13,9 +13,14 @@ namespace masodik
             Random veletlen = new Random();
             string[] lehetoseg = new string[] { "Kő", "Papír", "Olló" };
 
-            int gepValaszt = veletlen.Next(0,3);
+            int gepValaszt = veletlen.Next(0, 3);
 
-            Console.WriteLine("Gép választása: {0}",lehetoseg[gepValaszt]);
+            Console.WriteLine("Gép választása: {0}", lehetoseg[gepValaszt]);
+
+            int jatekosValasz;
+            Console.WriteLine("Kő (0) Papír(1) Olló(2)");
+            jatekosValasz = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Játéks választása: {0}",lehetoseg[jatekosValasz]);
             
 
 

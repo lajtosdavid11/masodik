625117519e5c551eaf69718c920befa970e91719
lajtosdavid11 <lajtos.david11@gmail.com>
2020-09-09 09:44:47 +0200

    Választások.

--- masodik/Program.cs
+++ masodik/Program.cs
@@ -21,6 +21,21 @@ namespace masodik
             Console.WriteLine("Kő (0) Papír(1) Olló(2)");
             jatekosValasz = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Játéks választása: {0}",lehetoseg[jatekosValasz]);
+
+            if (jatekosValasz == gepValaszt) //Döntetlen
+            {
+                Console.WriteLine("Döntetlen");
+            }
+
+            else if (jatekosValasz == 0 && gepValaszt == 1 || jatekosValasz == 1 && gepValaszt ==2 || jatekosValasz ==2 && gepValaszt ==0) //Gép nyer
+            {
+                Console.WriteLine("Gép nyert");
+
+            }
+            else //Játékos nyer
+            {
+                Console.WriteLine("Játékos nyert");
+            }
             
 
 
